24d068b5a6326053ac513407fa0967e417b07e40
larionit <illarionit@gmail.com>
2024-09-28 17:39:18 +0500

    Added a privilege upgrade to vps-setup-dockge.sh

--- vps-setup-dockge.sh
+++ vps-setup-dockge.sh
@@ -5,9 +5,32 @@
 dockge_compose=compose.yaml
 dockge_compose_url=https://dockge.kuma.pet/compose.yaml?port=5001&stacksPath=%2Fopt%2Fstacks
 
+# Link to this script (needed in case of privilege escalation via sudo)
+script_url=https://raw.githubusercontent.com/larionit/vps-setup/refs/heads/main/vps-setup-dockge.sh
+
 ### ======== Settings ======== ###
 
 ### -------- Functions -------- ###
+
+# Privilege escalation function
+function elevate {
+    if [ "$EUID" -ne 0 ]; then
+        echo "This script must be run with superuser privileges. Trying to elevate privileges with sudo."
+        exec sudo bash "$0" "$@"
+        exit 1
+    fi
+}
+
+# Privilege escalation function (if the script is run with a command like: "bash <(curl https://domain.name/script.sh)")
+function elevate_curl {
+    if [[ "$EUID" -ne 0 ]]; then
+        echo "This script must be run with superuser privileges. Trying to elevate privileges with sudo."
+        temp_script=$(mktemp)
+        curl -fsSL "$script_url" -o "$temp_script"
+        exec sudo bash "$temp_script" "$@"
+        exit 1
+    fi
+}
 
 # Find and replace function
 function find_and_replace {
@@ -20,6 +43,64 @@ function find_and_replace {
 }
 
 ### -------- Functions -------- ###
+
+### -------- Preparation -------- ###
+
+# Define the directory where this script is located
+script_dir="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+# Define the name of this script
+script_name=$(basename "$0")
+
+# Defining the directory name and script name if the script is launched via a symbolic link located in /usr/local/bin
+if [[ "$script_dir" == *"/usr/local/bin"* ]]; then
+    real_script_path=$(readlink ${0})
+    script_dir="$( cd -- "$(dirname "$real_script_path")" >/dev/null 2>&1 ; pwd -P )"
+    script_name=$(basename "$real_script_path")
+fi
+
+# Path to this script
+script_path="${script_dir}/${script_name}"
+
+# Path to this script with escaped slashes (for sed)
+script_path_sed=$(echo "$script_path" | sed 's/\//\\\//g')
+
+# Path to log file
+logfile_path="${script_dir}/${script_name%%.*}.log"
+
+# For console output
+echo_tab='     '
+show_ip=$(hostname -I)
+
+# Set the flag file name and location
+flag_file_resume_after_reboot="${script_dir}/resume-after-reboot-${script_name%%.*}"
+
+# Get user name
+script_was_started_by=$(logname)
+
+# Path to .bashrc
+bashrc_file="/home/${script_was_started_by}/.bashrc"
+
+# Privilege escalation
+if [[ "$script_dir" = *"/proc"* ]]; then
+    elevate_curl
+else
+    elevate
+fi
+
+echo
+echo "script_dir = $script_dir"
+echo
+echo "script_name = $script_name"
+echo
+echo "script_path = $script_path"
+echo
+read -p "Press Enter to contine: "
+
+# Start logging
+exec > >(tee -a "$logfile_path") 2>&1
+
+### -------- Preparation -------- ###
 
 curl "$dockge_compose_url" --output compose.yaml
 
